b0b25a5ca7323c2cea94c8428dcd61d1b69cee3d
nooraldeentumeh <90515044+Noortumeh@users.noreply.github.com>
2025-12-31 12:55:36 +0200

    build APIs for Admin dashboard

--- backend/app/Providers/ProfileProvider.php
+++ backend/app/Providers/ProfileProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
-use App\Services\{HelperServices, ProfileService};
+use App\Services\ProfileService;
 
 
 class ProfileProvider extends ServiceProvider implements DeferrableProvider
@@ -14,11 +14,7 @@ class ProfileProvider extends ServiceProvider implements DeferrableProvider
      * Register services.
      */
     public function register(): void
-    {
-        // App::bind('welcome', function () {
-        //     return new HelperServices();
-        // });
-        
+    {    
         App::bind(ProfileService::class, function () {
             return new ProfileService();
         });
--- backend/routes/admin.php
+++ backend/routes/admin.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function (Request $request) {
-    return "welcome";
-})->middleware('auth:sanctum')->middleware('CheckUserRole');
+Route::middleware(['auth:sanctum', 'CheckUserRole'])->group(function () {
+    Route::prefix('/admin')->group(function () {
+        Route::get('/users', [AdminController::class, 'getUsers']);
+        Route::get('/user', [AdminController::class, 'getUserData']);
+    });
+});
